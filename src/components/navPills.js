bf42abea5c9265fba1a9b9a9293d1669d4612343
amrismail123 <amresmel3@gmail.com>
2023-03-29 23:30:48 +0200

    onclick-content

--- src/components/navPills.js
+++ src/components/navPills.js
@@ -1,6 +1,9 @@
 import { Container,Row,Col,Nav,TabContainer } from "react-bootstrap";
 
 function NavPills (){
+    const Onclick=()=>{
+        document.getElementById("show-content").innerHTML="My name is Amr ,i live in cairo ,this is the 1st proj in react Dev.";
+    }
     return(
     <section className="project" id="project">
         <Container>
@@ -11,18 +14,22 @@ function NavPills (){
         <TabContainer id="projects-tabs" defaultActiveKey="first">  
         <Nav activeKey="/home" variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
             <Nav.Item>
-                <Nav.Link eventKey="first">1st Section</Nav.Link>
+                <Nav.Link  eventKey="first">1st Section</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="seconed">2nd Section</Nav.Link>
+                <Nav.Link  eventKey="seconed">2nd Section</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="third">3rd Section</Nav.Link>
+                <Nav.Link onClick={Onclick} eventKey="third">3rd Section</Nav.Link>
             </Nav.Item>
         </Nav>
         </TabContainer>  
                 </Col>
+                
             </Row>
+            <Col>
+                <div id="show-content"></div>
+            </Col>
         </Container>
     </section>  
     )
